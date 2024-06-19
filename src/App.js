5cdc19feeb883df9fbb87fa07da413668f59adf2
krishnamohan645 <krishnaguduri08@gmail.com>
2024-06-19 12:16:06 +0530

    admin_dashboard

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
 import Pie from "./scenes/pie";
-// import Line from "./scenes/line";
-// import Geography from "./scenes/geography";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -38,8 +38,8 @@ function App() {
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
